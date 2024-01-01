359d4c64fe7b30e4661ad2efb41fb6e3074af44b
Sniper10754 <Sniper10754@users.noreply.github.com>
2024-01-01 20:59:56 +0100

    .

--- compile_and_inspect.py
+++ compile_and_inspect.py
@@ -1,11 +1,53 @@
+#!/usr/bin/env python3
 import subprocess
 import tempfile
+import pathlib
 import sys
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        
+    file_to_compile = None
 
-    with tempfile.TemporaryFile() as output_temp_file:
+    if len(sys.argv) == 2:
+        file_to_compile = sys.argv[1]
+    else:
+        print(f"Usage: {sys.argv[0]} [PATH]")
+        exit(0)
 
+    with tempfile.NamedTemporaryFile() as output_temp_file:
+        result = subprocess.run([
+            "cargo",
+            "r",
+            "--quiet",
+            "--bin",
+            "frostbite-compiler-driver",
+            "--",
+            "compile",
+            str(
+                pathlib.Path(file_to_compile)
+                    .absolute()
+                ),
+            "-o",
+            str(
+                pathlib.Path(output_temp_file.name)
+                    .absolute()
+            )
+        ])
 
+        if result.returncode != 0:
+            exit(1)
+
+        subprocess.run([
+            "cargo",
+            "r",
+            "--quiet",
+            "--bin",
+            "frostbite-disassembler",
+            "--",
+            "disassemble",
+            str(
+                pathlib.Path(output_temp_file.name)
+                    .absolute()
+            )
+        ])
+
+    subprocess.call
